3b657144b581eca0f81916637efc94859c9b67af
Zac-L <Zachlichens@gmail.com>
2018-04-19 15:56:39 -0700

    took out body parser for expresses built in parser / url encode

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+// const bodyParser = require('body-parser');
 
+// ### Middleware ### //
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.json());
 
 
 // ### Required routes ### //
