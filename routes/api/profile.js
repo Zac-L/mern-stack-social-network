e249d65b84ed6f0a81c549b0da403af0a80186d8
Zac-L <Zachlichens@gmail.com>
2018-04-21 00:49:07 -0700

    initial route for getting a users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 
 router
-  .get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+  .get('/test', (req, res) => res.json({ msg: 'Profile Works' }))
+
+  .get('/', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user._id })
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(next);
+  });
 
 module.exports = router;
