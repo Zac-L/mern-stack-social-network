b27e9373a9565971194055a449159c0b051632ea
Zac-L <Zachlichens@gmail.com>
2018-04-22 16:27:01 -0700

    brought in error handling with in routes, a redirect to https for production and a check for the DB state

--- routes/app.js
+++ routes/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const errorHandler = require('../utils/error-handler');
+const redirectHttp = require('../utils/redirect-http');
+const checkDB = require('../utils/checkDB');
 const passport = require('passport');
 const app = express();
 
@@ -10,9 +13,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(passport.initialize());
 
+
 // ### Passport config ### //
 require('../passport')(passport);
 
+// ### Redirect http to https in production ### //
+if(process.env.NODE_ENV === 'production') {
+  app.use(redirectHttp);
+}
 
 // ### Required routes ### //
 const users = require('./api/users');
@@ -23,5 +31,10 @@ const posts = require('./api/posts');
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// ### Catchers ### //
+app.use(checkDB);
+app.use(errorHandler());
+
 
 module.exports = app;
---
+++ utils/error-handler.js
@@ -0,0 +1,35 @@
+/* eslint no-console: off */
+module.exports = function() {
+  
+  return function errorHandler(err, req, res, next) { //eslint-disable-line no-unused-vars
+
+    let code = 500, error = 'Internal Server Error';
+
+    // Mongoose Validation Error?
+    if(err.name === 'ValidationError') {
+      code = 400;
+      error = err.message;
+      console.log('ERROR', code, error);
+    }
+
+    else if(err.name === 'CastError') {
+      code = 404;
+      error = `id ${err.value} does not exist`;
+    }
+    // is this one of our errors?
+    else if(err.code) {
+      // by convention, we're passing in an object
+      // with a code property === http statusCode
+      // and a error property === message to display
+      code = err.code;
+      error = err.error;
+      console.log('ERROR', err.code, err.error);
+    }
+    // or something unexpected?
+    else {
+      console.log('ERROR*', err);
+    }
+
+    res.status(code).send({ error });
+  };
+};
